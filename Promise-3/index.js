1d2614a6101c4ef1972e738fb413d1beb14444ec
Andrew Pham <andrew.pham@shopify.com>
2023-09-13 12:07:41 -0400

    Finish promise 3

--- Promise-3/index.js
+++ Promise-3/index.js
@@ -1,7 +1,7 @@
 function greet(name) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (typeof name === 'string') { 
+      if (typeof name === 'string') {
         resolve('hello ' + name);
       } else {
         reject('Greet expects a string!');
@@ -23,12 +23,12 @@ function uppercaser(str) {
 }
 
 
-// Above we have two functions that return promises. 
-// These are greet and uppercaser. 
-// Notice below how we chain these promises together. The 
-// Result of one is then passed to the next. 
-// When chained all of the promises have to resolve for success. 
-// If any of the promises reject then you end in the catch block. 
+// Above we have two functions that return promises.
+// These are greet and uppercaser.
+// Notice below how we chain these promises together. The
+// Result of one is then passed to the next.
+// When chained all of the promises have to resolve for success.
+// If any of the promises reject then you end in the catch block.
 
 greet('Your name') // Returns a Promise
   .then(str => uppercaser(str))  // Upper case the results from greet() Returns a Promise
@@ -36,10 +36,21 @@ greet('Your name') // Returns a Promise
   .catch(err => console.log(err)) // Catches an error
 
 // Challenges: get greet() to fail by passing a non string value
-// What happens? 
+// What happens?
+greet(2)
+  .then(str => console.log(str))
+  .catch(err => console.log(err))
+
+  // When passed a number into greet it will reject and go to the catch block
 
 // Challenge: get uppercaser() to fail by passing a non string value
-// What happens? 
+// What happens?
+uppercaser(2)
+  .then(str => console.log(str))
+  .catch(err => console.log(err))
+  // When passed a number into uppercaser it will reject and go to the catch block
 
-// Challenge: Notice there is only a single .catch() at the end. 
+// Challenge: Notice there is only a single .catch() at the end.
 // Explain the behavior?
+
+// You only need one catch block at the end of the chain to catch any errors that occur in the chain.
